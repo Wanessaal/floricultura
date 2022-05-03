6b975152751a127504c52a519c7a64ed08c463b6
Wanessa <maria.freitas278@aluno.ce.gov.br>
2022-05-03 16:03:24 -0300

    pagina de login

---
+++ Pages/Login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Entrar</h1>
+    <from action=""method="POST">
+       <p>
+            <label>E-mail</label>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label>Senha</label>
+            <input type="text" password="senha">
+        </p>
+        <p> 
+            <button type="submit">Entrar</button>
+        <p>
+    </from>   
+</body>
+</html>
