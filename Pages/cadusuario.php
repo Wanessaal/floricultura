629f014575e4fdaff717c26779dfdd6532694346
Wanessa <maria.freitas278@aluno.ce.gov.br>
2022-05-09 14:52:09 -0300

    terminado

--- Pages/cadusuario.php
+++ Pages/cadusuario.php
@@ -4,7 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulario</title>
+</head>
+<body>
+    <div class="form">
+    <form method="post" action="index.php"><br>
+        <h1>Sistema de Usuario</h1><br>
+        <h2>Cadastro</h2><br><br>
+        <label for="nome">Nome:</label>
+        <input type="text"size="40"placeholder="Nome Completo">
+        <label for="cpf">CPF:</label>
+        <input type="text" placeholder="231.231.321-00"><br><br>
+        <label for="email">Email:</label>
+        <input type="text"size="40"placeholder="Email valido">
+</head>
 </head>
 <body>
     
