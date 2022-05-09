35aa6a796bc0ed0048f7ee042805b5a0f21a79ad
Wanessa <maria.freitas278@aluno.ce.gov.br>
2022-05-09 14:43:50 -0300

    codigo pronto

--- index.php
+++ index.php
@@ -6,18 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Floricultura</title>
-    <link rel="stylesheet" type="text/css"  href="estilo.css" />
+    <link rel="stylesheet" type="text/css"  href="./css/estilo.css" />
 </head>
 <body>
-<nav>
-	<ul class="menu">
-		<li><a href="./pages/login.php">Login</a></li>
-		<li><a href="./pages/cadusario.php">Cadastro de usuario </a></li>
-		<li><a href="./pages/cadcliente.php">Cadastro de clientes</a></li>
-		<li><a href="./pages/cadProduto.php">Cadastro de plantas</a></li>
-		<li><a href="./pages/paineldeescolha.php">Painel de escolha</a></li>  
-		<li><a href="./pages/forma de pagamento.php">forma de pagamento</a></li>		
-	</ul>
-</nav>     
+	<div class="container">
+		<nav>
+			<ul class="menu">
+				<li><a href="./pages/login.php">Login</a></li>
+				<li><a href="./pages/cadusario.php">Cadastro de usuario </a></li>
+				<li><a href="./pages/cadcliente.php">Cadastro de clientes</a></li>
+				<li><a href="./pages/cadProduto.php">Cadastro de plantas</a></li>
+				<li><a href="./pages/paineldeescolha.php">Painel de escolha</a></li>  
+				<li><a href="./pages/forma de pagamento.php">forma de pagamento</a></li>		
+			</ul>
+		</nav>    
+	</div>
 </body>
 </html>
