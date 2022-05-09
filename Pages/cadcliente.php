72c35682f271f31aa07189eb4890ab35ead7783c
Wanessa <maria.freitas278@aluno.ce.gov.br>
2022-05-09 14:42:14 -0300

    codigo pronto

---
+++ Pages/cadcliente.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <div class="form">
+    <form method="post" action="index.php"><br>
+        <h1>Sistema de Clientes</h1><br>
+        <h2>Cadastro</h2><br><br>
+        <label for="nome">Nome:</label>
+        <input type="text"size="40"placeholder="Nome Completo">
+        <label for="cpf">CPF:</label>
+        <input type="text" placeholder="231.231.321-00"><br><br>
+        <label for="email">Email:</label>
+        <input type="text"size="40"placeholder="Email valido">
+        <h2>Endereco</h2><br><br>
+        <label for="cidade">Cidade:</label>
+        <input type="text"placeholder=" Cidade"><br><br>
+        <label for="cep">CEP:</label>
+        <input type="text"placeholder="63100-000">
+        <label for="bairro">Bairro:</label>
+        <input type="text"placeholder="Bairro"><br><br>
+        <label for="rua">Rua:</label>
+        <input type="text"size="30"placeholder="logradouro">
+        <label for="nu">Número</label>
+        <input type="text"size="5"placeholder="Número"><br><br>
+        <input type="submit" value="Enviar">
+        <input type="button" value="Voltar">
+
+
+     </form>
+        +
+
+
+    
+</body>
+</html>
