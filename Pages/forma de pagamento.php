7f400aea65be744fed14b9687dd7149c6793e7e0
Wanessa <maria.freitas278@aluno.ce.gov.br>
2022-05-16 10:52:18 -0300

    terminando ainda

---
+++ Pages/forma de pagamento.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <div class="form">
+    <form method="post" action="index.php"><br>
+        <h1>Forma de pagamento </h1><br>
+         <h2>escolha</h2><br>
+         <h3>Pix, Cartao, dinheiro</h3>
+        <section class="pagamento">
+            <div class="forma-pagamento'>
+            <form method="POST">
+                <div class="form-100'>
+                <label>pix:</label>
+                <input type="text" />     
+                   </div>
+                   <div class="form-100">
+                     <label>Dados do Pix</label>
+                     <input type="text" placeholder=" Nome completo, Cpf, Numero do telefone,"/>
+                     <input type="text" placeholder="12/23" />]
+                     <input type="text" placehoder="cvc" />
+</div>
+<div class="form-100">
+   
+                    
+                       
+                
+    
+</body>
+</html>
