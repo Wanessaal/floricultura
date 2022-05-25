5706be546658040f8d91a351ba22ca3a1f7527d2
Herlyte <herlytereinaldo2004@gmail.com>
2022-05-25 10:51:26 -0300

    comecando

--- Pages/paginainicial.php
+++ Pages/paginainicial.php
@@ -3,3 +3,4 @@
         <title>D'Flores</title>
         <link href="..css/estilo.css/" rel="stylesheet" />
         <meta  name="viewport" content="Width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        
