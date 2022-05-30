25f2f2654bb24cc87945c7c88258f64450552d9a
Herlyte <herlytereinaldo2004@gmail.com>
2022-05-30 14:38:02 -0300

    terminando

--- Pages/Login.php
+++ Pages/Login.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/usuario.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Login</title>
 </head>
+
 <body>
-    <h1>Entrar</h1>
-    <form action="" method="POST">
-       <p>
-            <label>E-mail</label>
-            <input type="text" name="email">
-        </p>
-        <p>
-            <label>Senha</label>
-            <input type="password" name="senha">
-        </p>
-        <p> 
-            <button type="submit">Login</button>
-        </p>
-     </form>   
+
+    <div class="container">
+        <form action="" method="POST">
+        <h1>Entrar</h1>
+            <div class="form-group">
+               
+                <p>
+                    <label class="label-control" for="email">E-mail</label>
+                    <input class="form-control" type="text" name="email">
+                </p>
+                <p>
+                    <label class="label-control" for="senha">Senha</label>
+                    <input class="form-control" type="password" name="senha">
+                </p>
+                <p>
+                    <button class="form-control btn btn-secondary" type="submit">Login</button>
+                </p>
+        </form>
+    </div>
+    </div>
+
+
 </body>
+
 </html>
