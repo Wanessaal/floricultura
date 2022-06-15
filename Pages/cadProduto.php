ceec44ca2153719ceb3884f440d90a0d186b5a4f
Herlyte <herlytereinaldo2004@gmail.com>
2022-06-15 10:58:36 -0300

    terminou

--- Pages/cadProduto.php
+++ Pages/cadProduto.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/usuario.css">
+    <link rel="stylesheet" href="../css/cadproduto.css">
     <title>Cadastro de Produto</title>
-</head>
+</head> 
 <body>
     <div class="container">
         <form method="post" action="../controle/inserir.php">
--- Pages/cadusuario.php
+++ Pages/cadusuario.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/usuario.css">
+    <link rel="stylesheet" href="../css/cadastrocliente.css">
     <title>Cadastro de Usuário</title>
 </head>
 
@@ -17,6 +17,7 @@
             <h2>Cadastro de Usuário</h2>
             <div class="form-group">
                 <p>
+                
                     <label for="nome">Nome:</label>
                     <input type="text" class="form-control" name="nomeusuario" id="nomeusuario" placeholder="Nome Completo">
                 </p>
